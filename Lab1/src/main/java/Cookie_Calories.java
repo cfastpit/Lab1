a3001266750c8af8a30c7c8935d88c61eb5af561
0778430 <0778430@ACIS-TUTOR01-P.pittstate.edu>
2020-02-07 10:40:23 -0600

    add cookie and taxs

---
+++ Lab1/src/main/java/Cookie_Calories.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Cookie_Calories {
+
+    /*
+    A bag of cookies holds 40 cookies.
+    10 servings in the bag 
+    a serving equals 300 calories
+     lets the user enter the number of cookies they ate
+    then reports the total number of calories consumed
+     */
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter the amount of cookies eatten");
+        double number_Of_Cookies = keyboard.nextDouble();
+        final int BAG = 40;
+        final double SERVINGS_IN_BAG = 10;
+        final int COOKIE_CALORIES = 300;
+        double calories_consumed = number_Of_Cookies * COOKIE_CALORIES;
+        double servings = number_Of_Cookies / SERVINGS_IN_BAG;
+        double ate_In_Bag = number_Of_Cookies / BAG;
+
+        System.out.println("The number of cookies you ate\t" + number_Of_Cookies + " Cookies");
+        System.out.println("servings in a bag \t\t" + SERVINGS_IN_BAG + " Cookies");
+        System.out.println("1 cookie = \t\t\t" + COOKIE_CALORIES + " Calories");
+        System.out.println("You consume\t\t\t" + calories_consumed + " Calories");
+    }
+}
